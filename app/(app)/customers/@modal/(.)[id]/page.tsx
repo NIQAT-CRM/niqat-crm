2ab1d48478c55ec70f3e766b7a238574137a565f
NIQAT <abdelkader.reyad@niqat.com>
2026-07-25 11:52:12 +0300

    Add files via upload

---
+++ app/(app)/customers/@modal/(.)[id]/page.tsx
@@ -0,0 +1,4 @@
+// اعتراض /customers/[id] عند الانتقال من القائمة → يفتح الكارت فوقها كـ modal
+// نفس مكوّن الصفحة الكامل بالضبط (بدون تكرار المنطق)
+export { default } from "../../[id]/page";
+export const dynamic = "force-dynamic";
---
+++ app/(app)/customers/@modal/default.tsx
@@ -0,0 +1,4 @@
+// لا شيء عندما لا يوجد كارت مفتوح (مطلوب للـ parallel route)
+export default function Default() {
+  return null;
+}
